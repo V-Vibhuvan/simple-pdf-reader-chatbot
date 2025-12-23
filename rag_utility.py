293d4b2c21379fd78f62c2098b32dd795c56917c
V-Vibhuvan <vibhuvanv@gmail.com>
2025-12-23 18:07:17 +0530

    Reformat rag_utility.py for consistency

--- rag_utility.py
+++ rag_utility.py
@@ -7,7 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_classic.chains import RetrievalQA
+from langchain.chains import RetrievalQA
 
 #Load environment variables from .env file
 load_dotenv()
